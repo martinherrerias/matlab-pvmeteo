dc45428a2a25c4d97e2c1dd79bd668819d25c553
Martin <martin.herrerias@gmail.com>
2022-09-27 12:45:36 +0200

    Fix in Perez uncertainty. Another try to get consistent confidence intervals in FITSENSORPLANE. Add plot of histograms vs seed values.

--- models/fitsensorplane.m
+++ models/fitsensorplane.m
@@ -1,5 +1,5 @@
 function [azm,tilt,calib,albedo,info] = fitsensorplane(GTI,DHI,BNI,ENI,sunel,sunaz,albedo,varargin)
-% [AZM,TILT,CAL] = FITSENSORPLANE(T,LOC,GTI,GHI,DHI,BNI,SUNEL,SUNAZ,ALBEDO) - take a series of
+% [AZM,TILT,CAL] = FITSENSORPLANE(GTI,GHI,DHI,BNI,SUNEL,SUNAZ,ALBEDO,[TILT0,AZ0]) - take a series of
 %   measurements GTI from one or several tilted radiation sensors, and perform best-fit estimates
 %   of their orientation. That is, find the orientation of each sensor that minimizes the 
 %   differences:
@@ -14,9 +14,15 @@ function [azm,tilt,calib,albedo,info] = fitsensorplane(GTI,DHI,BNI,ENI,sunel,sun
     if nargin < 7, albedo = []; end
     knownalbedo = ~isempty(albedo) && isfinite(albedo);
     
-    [arg,varargin] = getflagoptions(varargin,{'-plot'});
+    % [arg,varargin] = getflagoptions(varargin,{'-plot'});
+    arg.seed_tilt = [];
+    arg.seed_az = [];
+    arg.metric = 'rms';
+    arg = parseoptions(varargin,{'-plot'},arg,'dealrest',2);
     
-    n = size(GTI,1);
+    arg.metric = parselist(arg.metric,{'mad','std','rms'});
+    
+    [n,Ns] = size(GTI);
     validateattributes(GTI,{'numeric'},{'real','2d'});
     cellfun(@(x,tag) validateattributes(x,{'numeric'},{'real','vector','size',[n,1]},'',tag),...
         {DHI,BNI,ENI,sunaz,sunel},{'DHI','BNI','ENI','sunaz','sunel'});
@@ -24,8 +30,14 @@ function [azm,tilt,calib,albedo,info] = fitsensorplane(GTI,DHI,BNI,ENI,sunel,sun
         validateattributes(albedo,{'numeric'},{'real','positive','vector'});
         compatiblesize(DHI,albedo);
     end
+    if ~isempty(arg.seed_tilt)
+        validateattributes(arg.seed_tilt,{'numeric'},{'real','>=',0,'<=',90,'vector','numel',Ns});
+    end
+    if ~isempty(arg.seed_az)
+        validateattributes(arg.seed_az,{'numeric'},{'real','>=',-180,'<=',180,'vector','numel',Ns});
+    end
     
-    filter = DHI > 10 & BNI >= -2 & sunel > 4;
+    filter = DHI > 10 & BNI > 0 & sunel > 4;
     if knownalbedo && ~isscalar(albedo), filter = filter & albedo >= 0; end
         
     GTI = GTI(filter,:);
@@ -36,10 +48,8 @@ function [azm,tilt,calib,albedo,info] = fitsensorplane(GTI,DHI,BNI,ENI,sunel,sun
     sunel = sunel(filter);
     if knownalbedo && ~isscalar(albedo), albedo = albedo(filter); end
     
-    Ns = size(GTI,2);
-
     % [F1,F2,sF1,sF2,rF1F2,Sg] = pvlmod_perezcoeffs(BNI,DHI,ENI,sunel,varargin{:});
-    [F1,F2] = pvlmod_perezcoeffs(BNI,DHI,ENI,sunel,varargin{:});
+    [F1,F2] = pvlmod_perezcoeffs(BNI,DHI,ENI,sunel);
 
     % Allow dot-product-like behaviour for arrays of any (compatible) size, without creating
     % or processing unnecessary data copies:
@@ -52,8 +62,9 @@ function [azm,tilt,calib,albedo,info] = fitsensorplane(GTI,DHI,BNI,ENI,sunel,sun
     % if k is a unit vector pointing towards the zenith, k'u = U{3}, k's = S{3}
     GHI = max(0,DHI + BNI.*S{3});
 
+    b = max(0.087,S{3});
     ISOh = (1-F1).*DHI;                 % Isotropic component, horizontal, ISOt = ISO·(1 + k'u)/2
-    CSn = F1.*DHI./max(0.087,S{3});     % Circumsolar component, normal, CSt = CSn·max(0,u's)
+    CSn = F1.*DHI./b;     % Circumsolar component, normal, CSt = CSn·max(0,u's)
     HBp = F2.*DHI;                      % Horizon-brightening, vertical, HBt = HBp·sqrt(1-(k'u)²)
     ALBc = GHI;                         % Albedo, vertical, ALBt = rho·ALB·(1 - k'u)/2
     % ALBc = GHI.*(1+sind((90-sunel)/2).^2);
@@ -63,92 +74,147 @@ function [azm,tilt,calib,albedo,info] = fitsensorplane(GTI,DHI,BNI,ENI,sunel,sun
     
     % Define a space over [tilt,azimuth] for each sensor
     x0 = seedvalues(GTI,S,CSn,HBp,BNI,ISOh,ALBc*albedo);
+    
+    knownseeds = ~isempty(arg.seed_az) && ~isempty(arg.seed_tilt);
+    if knownseeds
+        c = dot(sph2cartV(x0(:,2),90-x0(:,1)),sph2cartV(arg.seed_az,arg.seed_tilt),2);
+        if any(abs(c) < 0.7)
+            warning('You might want to check your sensor/data azimuth convention')
+        else
+           x0 = [arg.seed_tilt(:),arg.seed_az(:)];
+        end
+    end
+
     x0 = x0(:)';
     
     lb = -Inf(1,2*Ns);
     ub = Inf(1,2*Ns);
         
     calib = ones(1,Ns);
-    opt = optimset('display','off','maxfunevals',1000,'TolX',sind(0.1),'TolFun',1e-4);
+    w = [];
 
     if knownalbedo
         ALBc = ALBc.*albedo;
-        costfcn = @(x) GTI_error(x(1:Ns),x(Ns+1:2*Ns),1);
+        errorfcn = @(x) GTI_error(x(1:Ns),x(Ns+1:2*Ns),1);
     else   
         x0 = [x0,0.2];
         lb = [lb,0];
         ub = [ub,1];
-        costfcn = @(x) GTI_error(x(1:Ns),x(Ns+1:2*Ns),x(end));
+        errorfcn = @(x) GTI_error(x(1:Ns),x(Ns+1:2*Ns),x(end));
     end
     
-    XGROUPS = 50; % rough DF of Perez model... doesn't matter too much
-    n = size(GTI,1);
-    gidx = discretize(rand(n,1),XGROUPS);
-    
-    [x,info.resnorm,info.resid,info.exitflag,info.output,info.lambda,info.J] = lsqnonlin(@(x) splitapply(@std,costfcn(x),gidx),x0,lb,ub,opt);
-    info.confidence_intervals = nlparci(x,info.resid,'jacobian',info.J);
-    
+    switch arg.metric
+    case 'mad', costfcn = @mad;
+    case 'std', costfcn = @std;
+    case 'rms', costfcn = @(x) sqrt(mean(x.^2));
+    end
+        
+    opt = optimset('display','off','maxfunevals',1000,'TolX',sind(0.1),'TolFun',1e-4,'DiffMinChange',1e-1);
+
+    [x,info.resnorm,info.resid,info.exitflag,info.output,info.lambda,info.J] = ...
+            lsqnonlin(@(x) errorfcn(x),x0,lb,ub,opt);
+
     tilt = x(1:Ns);
     azm = x(Ns+1:2*Ns);
+    
+    % TODO: consult a statistician.
+    % Applying weights to errors (1 = fully-directional source on the plane of the sensor)
+    % and using df = sum(w) yields relatively consistent confidence bounds for the same data set 
+    % at different resolutions, but is theoretically shaky.
+    % The same goes for accounting for biases due to transposition.
+    
+        % Estimate of Perez error, due to coefficients only (standard error set to 0)
+        [~,~,~,~,~,~,Se] = pvlmod_perez(tilt,azm,DHI,BNI,ENI,sunel,sunaz,albedo,[],[],[],0);
+
+        % NLPARCI will assume errors are independent, i.e. divide by sqrt(n), but errors due to
+        % transposition are likely to be consistent biases. Multiplying by sqrt(n) should account
+        % for the worst-case perfect correlation.
+        Se = hypot(Se.*w./vecnorm(w)*sqrt(n),info.resid).*sqrt(n./sum(w,1));
+
+    info.confidence_intervals = nlparci(x,Se,'jacobian',info.J);
 
     badconv = tilt < 0;
     tilt(badconv) = -tilt(badconv);
     azm(badconv) = azm(badconv)+180;
     
     if ~knownalbedo, albedo = x(end); end
-    
-    % Refine 2nd order finite differences
-    DELTA_MIN = [2,45];
-    delta = min(max(diff(info.confidence_intervals(1:2*Ns,:),1,2)/2,DELTA_MIN(1)),DELTA_MIN(2));
-    delta = reshape(delta,[],2)';
-    
-    E = zeros(3,3,Ns);
-    for j = 1:3
-        for k = 1:3
-            z = tilt + (j-2)*delta(1,:);
-            y = azm + (k-2)*delta(2,:);
-            e = GTI_error(z,y,albedo);
-            E(j,k,:) = std(e,1);
-        end
+
+    DELTA_MIN = 2;
+    if knownseeds
+        c = dot(sph2cartV(azm,90-tilt),sph2cartV(arg.seed_az,90-arg.seed_tilt),2);
+        DELTA_MIN = max(DELTA_MIN,acosd(min(c))*1.5);
     end
-    delta(1,:) = delta(1,:)./sqrt(squeeze(diff(E(:,2,:),2,1)))';
-    delta(2,:) = delta(2,:)./sqrt(squeeze(diff(E(2,:,:),2,2)))';
-    delta(2,:) = min(delta(2,:),180);
-    
-    info.confidence_intervals = x(:) + [-1,1].*delta(:);
+    delta = max(diff(info.confidence_intervals,1,2),DELTA_MIN);
+    delta = reshape(delta,[],2)';
  
+    e0 = GTI_error(tilt,azm,albedo,0);
+    % c0 = costfcn(e0);
+    % df = sum(w);
     if arg.plot
+        colors = get(gca,'DefaultAxesColorOrder');
+        
         [xx,yy] = ndgrid(linspace(-1,1,11),linspace(-1,1,11));
         E = zeros(numel(xx),Ns);
         for j = 1:numel(xx)
             x = tilt + xx(j)*delta(1,:);
             y = azm + yy(j)*delta(2,:);
             e = GTI_error(x,y,albedo);
-            E(j,:) = std(e,1);
+            
+            % F = c0/costfcn(e);
+            % E(j,:) = 2*fcdf(F,df,df);
+            E(j,:) = costfcn(e);
         end
         E = reshape(E,[size(xx),Ns]);
 
-        GUIfigure('fitsensorplane');
-        ax = arrayfun(@(j) subplot(1,Ns,j),1:Ns);
+        GUIfigure('fitsensorplane','fitsensorplane',sprintf('%d:%d',Ns,1+knownseeds)); clf();
+        if knownseeds
+            ax = arrayfun(@(j) subplot(2,Ns,j),1:2*Ns);
+            histopts =  {'normalization','count','displaystyle','stairs'};
+        else
+            ax = arrayfun(@(j) subplot(1,Ns,j),1:Ns);
+        end
+
         for k = 1:Ns
             imagesc(ax(k),tilt(k) + xx(:,1)*delta(1,k),azm(k) + yy(1,:)*delta(2,k),E(:,:,k))
             
+            hold(ax(k),'on');
+            plot(ax(k),tilt(k),azm(k),'+','color',colors(1,:),'markersize',10,'linewidth',2);
             xlabel(ax(k),'tilt (deg)')
             ylabel(ax(k),'azimuth (deg)')
             title(ax(k),sprintf('GTI_%d',k))
             h = colorbar(ax(k));
-            h.Label.String = 'STD [W/m^2]';
-        end
+            h.Label.String = [upper(arg.metric) ' [W/m^2]'];
+            
+            if knownseeds
+                plot(ax(k),arg.seed_tilt(k),arg.seed_az(k),'+','color',colors(2,:),'markersize',10,'linewidth',2);
+                e = GTI_error(arg.seed_tilt(k),arg.seed_az(k),albedo,0);
+                hold(ax(k+Ns),'on');
+                w0 = prctile(w(:,k),75);
+                ff = w(:,k) > w0;
+                ylabel(ax(k+Ns),sprintf('samples with w > %0.2f',w0));
+                H = histogram(ax(k+Ns),e0(ff),histopts{:},'DisplayName','Optimized');
+                histogram(ax(k+Ns),e(ff),histopts{:},'BinEdges',H.BinEdges,'DisplayName','Nominal');
+                histogram(ax(k+Ns),e(ff & sunaz > 0),histopts{:},'BinEdges',H.BinEdges,'edgecolor',colors(2,:),'LineStyle',':','DisplayName','Morning');
+                histogram(ax(k+Ns),e(ff & sunaz < 0),histopts{:},'BinEdges',H.BinEdges,'edgecolor',colors(2,:),'LineStyle','--','DisplayName','Evening');
+                legend(ax(k+Ns),'box','off');
+                xlabel(ax(k+Ns),'GTI_{mdl} - GTI_{mes} [W/m^2]');
+            end
+        end   
     end
 
-    function E = GTI_error(surftilt,surfaz,albedo)
+    function E = GTI_error(surftilt,surfaz,albedo,tweaked)
+        
+        if nargin < 4, tweaked = true; end
+        
         U = sph2cartC(surfaz,90-surftilt);
         
-        a = max(0,dotC3(S,U));       % a = max(0,u's) 
-        ISO = ISOh.*(1 + U{3})/2;    %	Isotropic component: (1-F1)·DHI·(1 + k'u)/2	
-        CS = CSn.*a;                 % Circumsolar component: F1·DHI·a/b
-        HB = HBp.*hypot(U{1},U{2});  % Hor. brightening component: F2·DHI·sqrt(1-(k'u)²) 
-        BTI = BNI.*a;                % Beam component
+        a = max(0,dotC3(S,U));      % a = max(0,u's) 
+        F_iso = (1 + U{3})/2;
+        F_hb = hypot(U{1},U{2});
+        ISO = ISOh.*F_iso;          %	Isotropic component: (1-F1)·DHI·(1 + k'u)/2	
+        CS = CSn.*a;                % Circumsolar component: F1·DHI·a/b
+        HB = HBp.*F_hb;             % Hor. brightening component: F2·DHI·sqrt(1-(k'u)²) 
+        BTI = BNI.*a;               % Beam component
 
         ALB = albedo*ALBc.*(1 - U{3})/2;
         GTIm = ISO + CS + HB + ALB + BTI;
@@ -160,13 +226,24 @@ function [azm,tilt,calib,albedo,info] = fitsensorplane(GTI,DHI,BNI,ENI,sunel,sun
         end
         
         E = double(ISO + CS + HB + ALB + BTI - GTI.*calib);
-        E(~isfinite(E)) = 0;
+                
+        if tweaked
+            w = 1-dotC3(S,U).^2;    % Give more weight to points near the sensor plane
+            w = w.*BTI./GTIm;       % ... with strong directional fraction
+            % w = w.^2;
+            E = E.*double(w);
+        end
+        
+        switch arg.metric
+        case 'mad', if tweaked, E = sign(E).*sqrt(abs(E)); end
+        case 'std', E = E - mean(E);
+        case 'rms'
+        end
 
-%         A = DHI.*(a./b - (1 + U{3})/2);
-%         B = DHI.*hypot(U{1},U{2});
-% 
-%         % Does not include covariance (several sensors at the same time)!
-%         Se = sqrt(Sg.^2+(sF1.*A).^2+(sF2.*B).^2 + 2*rF1F2.*A.*B.*sF1.*sF2);
+        bad = ~isfinite(E);
+        if any(bad)
+            E(bad) = mean(E(~bad));
+        end
     end
 end
 
